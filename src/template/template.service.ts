95a5bdc4cf517a0a63f0f4bc40f1e2aa315366f9
Riccardo Pirani <riccardo.pirani@reiser.it>
2024-08-01 08:59:58 +0200

    backend app
    backend app

--- src/template/template.service.ts
+++ src/template/template.service.ts
@@ -5,7 +5,6 @@ import { Repository } from "typeorm";
 import { CreateTemplateDto } from "./dto/create-template.dto";
 import { UpdateTemplateDto } from "./dto/update-template.dto";
 import { Template } from "./entities/template.entity";
-
 const AdmZip = require("adm-zip");
 
 @Injectable()
@@ -98,6 +97,11 @@ export class TemplateService {
     <style>
         * {
             box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+        }
+        html, body {
+            height: 100%;
         }
         body {
             font-family: Verdana, sans-serif;
@@ -105,300 +109,53 @@ export class TemplateService {
             background-size: cover;
             background-repeat: no-repeat;
             background-position: center;
-        }
-        .mySlides {
-            display: none;
-        }
-        img {
-            vertical-align: middle;
+            min-height: 100%;
         }
         .slideshow-container {
             max-width: 400px;
             position: relative;
             margin: auto;
+            text-align: right; /* Align the text to the right */
+            height: 100%; /* Ensure the container takes the full available height */
+            display: flex;
+            flex-direction: column;
+            align-items: flex-start; /* Align items to the start (left) */
+            justify-content: center; /* Center items vertically */
         }
-        .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
+        .slideshow-container img {
+            margin: 10px;
+            width: 150px; /* Adjust width as needed */
+            height: 200px; /* Adjust height as needed */
+            border-radius: 50%; /* Make the image oval */
         }
-        .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
+        .slideshow-container h2 {
+            margin-top: 10px;
+            text-align: left; /* Align the title to the left */
+            color: black; /* Set text color to black */
         }
-        .dot {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-        }
-        .active {
-            background-color: #717171;
-        }
-        .fade {
-            animation-name: fade;
-            animation-duration: 1.5s;
-        }
-        @keyframes fade {
-            from {
-                opacity: 0.4;
-            }
-            to {
-                opacity: 1;
-            }
+        .slideshow-container p {
+            text-align: right; /* Align the paragraph to the right */
+            color: black; /* Set text color to black */
         }
         @media only screen and (max-width: 300px) {
-            .text {
-                font-size: 11px;
+            .slideshow-container h2, .slideshow-container p {
+                font-size: 11px; /* Adjust font size for small screens */
             }
         }
     </style>
 </head>
 <body>
-    <center>
-        <h2>${nome}</h2>
-        <p>Messaggio: ${testotemp}</p>
-        <div class='slideshow-container'>
-            <img src='https://www.marconisoftware.com/assets/01.png' style='width: 100%' />
-        </div>
-    </center>
+    <div class='slideshow-container'>
+        <img src="image.png" alt="Image" style="border-radius: 30%;margin-right: 30%" > <!-- Ensure the image displays properly and is oval -->
+        <h2  style="margin-right: 30%">${nome}</h2> <!-- Name below the image -->
+        <div style="margin-left: 60%"> <p>Messaggio: ${testotemp}</p></div>
+    </div>
 </body>
 </html>
-`;
-    } else if (templateType === TemplateType.TEMPLATE2) {
-      let base64Image = image1.split(";base64,").pop();
-      fs.writeFile(
-        `${dir}/image.png`,
-        base64Image,
-        { encoding: "base64" },
-        function (err) {
-          console.log(err);
-        }
-      );
-      templateHTML = `
-<!DOCTYPE html>
-<html>
-<head>
-    <title>1</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1' />
-    <style>
-        * {
-            box-sizing: border-box;
-        }
-        body {
-            font-family: Verdana, sans-serif;
-            background-image: url('https://www.marconisoftware.com/assets/01.png');
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-        }
-        .mySlides {
-            display: none;
-        }
-        img {
-            vertical-align: middle;
-        }
-        .slideshow-container {
-            max-width: 400px;
-            position: relative;
-            margin: auto;
-        }
-        .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
-        }
-        .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
-        }
-        .dot {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-        }
-        .active {
-            background-color: #717171;
-        }
-        .fade {
-            animation-name: fade;
-            animation-duration: 1.5s;
-        }
-        @keyframes fade {
-            from {
-                opacity: 0.4;
-            }
-            to {
-                opacity: 1;
-            }
-        }
-        @media only screen and (max-width: 300px) {
-            .text {
-                font-size: 11px;
-            }
-        }
-    </style>
-</head>
-<body>
-    <center>
-        <h2>${nome}</h2>
-        <p>Messaggio: ${testotemp}</p>
-        <div class='slideshow-container'>
-            <img src='https://www.marconisoftware.com/assets/01.png' style='width: 100%;height: 100%' />
-        </div>
-    </center>
-</body>
-</html>
-`;
-    } else {
-      let base64Image = image1.split(";base64,").pop();
-      let base64Image2 = image2.split(";base64,").pop();
-      let base64Image3 = image3.split(";base64,").pop();
-      fs.writeFile(
-        `${dir}/image.png`,
-        base64Image,
-        { encoding: "base64" },
-        function (err) {
-          console.log(err);
-        }
-      );
-      fs.writeFile(
-        `${dir}/image2.png`,
-        base64Image2,
-        { encoding: "base64" },
-        function (err) {
-          console.log(err);
-        }
-      );
-      fs.writeFile(
-        `${dir}/image3.png`,
-        base64Image3,
-        { encoding: "base64" },
-        function (err) {
-          console.log(err);
-        }
-      );
+ `;
 
-      templateHTML = `<!DOCTYPE html>
-      <html>
-      <head>
-        <title>Memoryp</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <style>
-          * {
-            box-sizing: border-box;
-          }
-          body {
-            font-family: Verdana, sans-serif;
-          }
-          .mySlides {
-            display: none;
-          }
-          img {
-            vertical-align: middle;
-          }
-          .slideshow-container {
-            max-width: 400px;
-            position: relative;
-            margin: auto;
-          }
-          .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
-          }
-          .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
-          }
-          .dot {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-          }
-          .active {
-            background-color: #717171;
-          }
-          .fade {
-            animation-name: fade;
-            animation-duration: 1.5s;
-          }
-          .column {
-            float: left;
-            width: 50%;
-            padding: 10px;
-            height: 300px;
-          }
-          .row:after {
-            display: table;
-            clear: both;
-          }
-          @keyframes fade {
-            from {
-              opacity: 0.4;
-            }
-            to {
-              opacity: 1;
-            }
-          }
-          @media only screen and (max-width: 300px) {
-            .text {
-              font-size: 11px;
-            }
-          }
-        </style>
-      </head>
-      <body style="background-color:#9dc3e7;">
-        <body>
-          <img src="image3.png" style="width: 100%" />
-          <p></p>
-          <div class="row">
-            <div class="column">
-              <p><img src="image.png" style="width: 50%" /></p>
-              <b>${nome}</b>
-            </div>
-            <div class="column">
-              <p>${testotemp}</p>
-              <p><img src="image2.png" style="width: 50%" /></p>
-            </div>
-          </div>
-          <div style="position: absolute; top: 80%; left: 0; bottom: 0; right: 0; width: auto; height: auto; max-width: 100%;max-height: 100%; margin: auto auto 0;">
-            <img src="image3.png" style="width: 100%" />
-          </div>
-        </body>
-      </html>`;
+
+
     }
 
     if (templateHTML.length > 0) {
